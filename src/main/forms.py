c6db6455baea77d53426f36d562cc087453ffb55
Suguru Chhaya <suguruchhaya@gmail.com>
2020-10-10 18:25:43 -0400

    create django project

--- src/main/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class CreateNewList(forms.Form):
-    name = forms.CharField(label="Name", max_length=200)
-    #!Can finish form without requirement
-    check = forms.BooleanField(required=False)
